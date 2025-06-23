387cbf194a8dd9c14e80048c8e09268f05f86272
Michel <fistonkajibwami2@gmail.com>
2025-06-22 21:46:13 -0330

    fixed link

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://backendaquo-production.up.railway.app/api',
+        target: 'https://backendaquo-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
