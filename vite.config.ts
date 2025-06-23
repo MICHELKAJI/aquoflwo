a6ae49eaee2f4e53c499b728c0741c49621c2ac8
Michel <fistonkajibwami2@gmail.com>
2025-06-22 21:11:06 -0330

    change link

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://backendaquo-production.up.railway.app/api',
         changeOrigin: true,
         secure: false,
       }
