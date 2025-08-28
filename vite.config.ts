fc9bb15d25cedb15192174a8b6ad8cc54813b73b
Michel <fistonkajibwami2@gmail.com>
2025-08-28 17:35:46 -0330

    fixed link

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://backendaquo.onrender.com/api',
+        target: 'https://backendaquo.onrender.com',
         changeOrigin: true,
         secure: false,
       }
